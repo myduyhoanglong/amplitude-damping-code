98eee7b4b4242b9424fb424ede04e2c78e1958f7
My Duy Hoang Long <myduyhoanglong@gmail.com>
2022-01-10 16:56:25 +0800

    Add files via upload

--- tomography.py
+++ tomography.py
@@ -1,19 +1,37 @@
 # process tomography
+import numpy as np
 
 from ec_unit import extended_memory, initialize
 from utils import *
-
-# import numpy as np
+import pickle
+import matplotlib.pyplot as plt
 
 s0 = np.array([[1., 0], [0, 1]])
 s1 = np.array([[0., 1], [1, 0]])
 s2 = np.array([[0., -1j], [1j, 0]])
 s3 = np.array([[1., 0], [0, -1]])
 
+p_power = [4.5, 4.4, 4.3, 4.2, 4.1, 4.0, 3.9, 3.8, 3.7, 3.6, 3.5, 3.4, 3.3, 3.2, 3.1, 3.0]
+
 
 def main():
+    with open("chi", "rb") as fp:
+        chi = pickle.load(fp)
+    p = np.power(np.array([10]), -1*np.array(p_power))
+    print(len(chi))
+    arr = []
+    for i in range(len(chi)):
+        arr.append(chi[i][1][1])
+    fig, ax = plt.subplots()
+    ax.plot(p, arr, marker='o', markerfacecolor='blue', markersize=7, color='skyblue', linewidth=3)
+    ax.set_yscale('log')
+    ax.set_xscale('log')
+    plt.show()
+
+
+def collect_chi():
     l = 0.5 * np.array([[1, 0, 0, 1], [0, 1, 1, 0], [0, 1, -1, 0], [1, 0, 0, -1]]).astype(np.float)
-    p_power = [4.5, 4.4, 4.3, 4.2, 4.1, 4.0, 3.9, 3.8, 3.7, 3.6, 3.5, 3.4, 3.3, 3.2, 3.1, 3.0]
+    chi = []
     for i in p_power:
         p = 10 ** (-i)
         print(p)
@@ -29,8 +47,11 @@ def main():
         s = [rho[0], rho[2] - 1j * rho[3] - 0.5 * (1 - 1j) * (rho[0] + rho[1]),
              rho[2] + 1j * rho[3] - 0.5 * (1 + 1j) * (rho[0] + rho[1]), rho[1]]
         s = np.concatenate((np.concatenate((s[0], s[1]), axis=1), np.concatenate((s[2], s[3]), axis=1)), axis=0)
-        chi = np.matmul(l, np.matmul(s, l)).real
-        print(chi)
+        chi_p = np.matmul(l, np.matmul(s, l)).real
+        print(chi_p)
+        chi.append(chi_p)
+    with open('chi', 'wb') as writer:
+        pickle.dump(chi, writer)
 
 
 def state_from_bloch(v):
